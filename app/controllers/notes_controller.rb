f34b4faadc4b90ba74910d46a1da1796ab3a575c
bhagathreddy123 <katta_reddy04@yahoo.com>
2015-06-13 00:07:32 +0530

    crud operations of note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,13 +1,53 @@
 class NotesController < ApplicationController
+ 
+ before_action :set_note, :except => [:index,:new, :create]
+ before_action :authenticate_user!, :except => [:index,:show]
+ 
   def index
+  	@notes = Note.all
   end
 
   def new
+  	@note = Note.new
   end
+
+  def create
+  	@note = Note.new(note_params)
+  	if @note.save
+  		redirect_to notes_path
+  	else
+  		render :new
+  	end
+  end
+
 
   def edit
   end
 
   def show
   end
+  
+   def update
+  	if @note.update(note_params)
+  		redirect_to notes_path
+  	else
+  		render :new
+  	end
+  end
+ 
+ def destroy
+ 	@note.destroy
+ 	redirect_to notes_path
+ end
+
+private
+
+def set_note
+	@note = Note.find(params[:id])
+end
+
+def note_params
+	params.require(:note).permit!
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
+  #root 'notes#index'
   resources :notes
 
 end
